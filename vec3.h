1d587d0011d4dd4a07a417064975bc0a05f38c10
Lewis Wardman <lewis2304@hotmail.co.uk>
2023-05-30 12:56:45 +0100

    Documented vec3.h

--- vec3.h
+++ vec3.h
@@ -15,6 +15,7 @@ public:
 
     vec3(double x, double y, double z) : _x(x), _y(y), _z(z) {}
 
+    // Handles overwriting of this vector with another vector.
     vec3& operator= (const vec3& otherVec3)
     {
         if (this == &otherVec3)
@@ -29,6 +30,7 @@ public:
         return *this;
     }
 
+    // Handles addition of another vector to this vector.
     vec3& operator+= (const vec3& otherVec3)
     {
         if (this == &otherVec3)
@@ -43,6 +45,7 @@ public:
         return *this;
     }
 
+    // Handles subtraction of another vector to this vector.
     vec3& operator-= (const vec3& otherVec3)
     {
         if (this == &otherVec3)
@@ -57,14 +60,68 @@ public:
         return *this;
     }
 
+    /**
+     * Prints out the coordinates of this vector in order : x, y, z
+     * Useful for debugging and sanity checks on what the current vector is.
+     */
     void printCoords() const;
 
+    /**
+     * * The magnitude of the vector is the 'length' of the vector.
+     *
+     * * For example, The magnitude of (1, 1, 1) is the distance of this point away from the origin.
+     *
+     * * The magnitude is calculated as the square root of the sum of squares of each vector entry.
+     *   e.g (1, 2, 3) has magnitude of sqrt(1*1 + 2*2 + 3*3).
+     */
     [[nodiscard]] double getMagnitude() const;
 
+    /**
+     * * Prints out the magnitude calculated in getMagnitude().
+     *
+     * * Useful for debugging and testing whether normalize() has done it's job/
+     *
+     * * Also used in the normalize() function.
+     */
+    void printMagnitude() const;
+
+    /**
+     * * normalizes the current vector. Replaces the member variables of the current class.
+     *
+     * * If the current vector wants to be kept,
+     *   initialise a new vec3 based on the current vector and normalize that instead.
+     *
+     * * Normalization is done by dividing each entry in the vector by the magnitude of the vector.
+     *   e.g (1, 1, 1) becomes (0.57735, 0.57735, 0.57735).
+     */
     void normalize();
 
+    /**
+     * * Calculates the angle between two vectors via the dot product.
+     *   Specifically uses the formula, cos(theta) = a.b / |a|*|b|. Where a.b is the dot product.
+     *
+     * * Converts the angle to degrees.
+     *
+     * @param firstVec the first vector to calculate with.
+     * @param secondVec the second vector to compare the first vector with.
+     * @returns The angle in degrees between the two vectors.
+     */
     static double getAngleBetween(const vec3& firstVec, const vec3& secondVec);
 
+    /**
+     * * Creates a new vector which is perpendicular to the two input vectors.
+     *   In 3d this is always possible unless the two input vectors are the same.
+     *
+     * * The perpendicular vector is calculated by the cross product.
+     *
+     * * The output vector can be tested by using getAngleBetween() for all three pairs of vectors.
+     *
+     * * The two input vectors do not need to be perpendicular to each other.
+     *
+     * @param firstVec The first vector to use in the cross product.
+     * @param secondVec The second vector to use in the cross product.
+     * @returns A perpendicular vector.
+     */
     static vec3 getPerpendicularVector(const vec3& firstVec, const vec3& secondVec);
 
 
