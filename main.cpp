c2751d481a23f9660f6776e8b4ba45ffdfba34aa
LWardman <lewis2304@hotmail.co.uk>
2023-09-20 14:20:28 +0200

    Provides an example of how to use the vector class. This is done by creating two vectors and obtaining perpendicular vectors, angle between vectors and normalisation.

--- main.cpp
+++ main.cpp
@@ -1,37 +1,22 @@
 #include <iostream>
-#include "vec3.h"
+#include "Vector_lib/vec3.h"
 
+using vector = vec3;
 
 int main()
 {
-    // Initialising two test vectors, these will be used to test other features in the vec3 class.
-    vec3 firstVector{ 4, 2, 9 };
-    firstVector.printCoords();
+    vector u = vector{ 1, 0, 0 } + vector{ 0, 2, 0 };
 
-    vec3 secondVector{ 1, 2, 3 };
-    secondVector.printCoords();
+    vector v = vector{ 0, 7, 0} - vector{4, 0, 0};
 
-    // Testing the class' ability to get the magnitude and divide each entry by the magnitude.
-    // It's expected that the magnitude after normalising is 1.
-    secondVector.printMagnitude();
+    vector perp = vector::getPerpendicularVector(u, v); // perp = {0, 0, 15}
 
-    secondVector.normalize();
+    perp = perp.normalize(); // perp = {0, 0, -1}
 
-    secondVector.printCoords();
+    std::cout << "Angle between u and perp = " << vector::getAngleBetween(u, perp) << std::endl;
+    std::cout << "Angle between v and perp = " << vector::getAngleBetween(v, perp) << std::endl;
 
-    secondVector.printMagnitude();
-
-    // Testing the class' ability to generate a perpendicular vector.
-    std::cout << "Angle between first and second vector : " << vec3::getAngleBetween(firstVector, secondVector) << std::endl;
-
-    std::cout << "Perpendicular vector to first and second vector : " << std::endl;
-
-    vec3 perpVec = vec3::getPerpendicularVector(firstVector, secondVector);
-
-    std::cout << "Angle between first and perp vector : " << vec3::getAngleBetween(firstVector, perpVec) << std::endl;
-    std::cout << "Angle between second and perp vector : " << vec3::getAngleBetween(secondVector, perpVec) << std::endl;
-
-    vec3::getPerpendicularVector(firstVector, secondVector).printCoords();
+    perp.printCoords();
 
     return 0;
 }
